137fbf7fa7c53fb160f53ac662cf79e0a06126c6
Maksim P <email@example.comkfc.kurochka@gmail.com>
2025-02-05 04:25:35 +0600

    config

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://147.45.157.15:8000/api/:path*', // ваш API URL
+        destination: 'http://147.45.157.15:8000/api/v1/shop/:path*', // ваш API URL
+      },
+      {
+        source: '/storage/:path*',
+        destination: 'http://147.45.157.15:8000/v1/shop/storage/:path*', // путь к изображениям
       },
       {
         source: '/storage/:path*',
