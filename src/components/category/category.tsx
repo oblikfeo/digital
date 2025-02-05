8bfbcc407be4479fb1b000867cb9a596017cd129
Maksim P <email@example.comkfc.kurochka@gmail.com>
2025-02-06 05:28:24 +0600

    +mobile catalog

--- src/components/category/category.tsx
+++ src/components/category/category.tsx
@@ -3,7 +3,7 @@ import styles from "./category.module.css"
 import CategoryMain from "./categoryMain/categoryMain"
 import axios from "axios"
 
-export default function CategoryMobile({ setCatalogButtonIpad }) {
+export default function CategoryMobile() {
 
     const [close, setClose] = useState(false)
     const [category, setCategory] = useState([])
@@ -19,7 +19,6 @@ export default function CategoryMobile({ setCatalogButtonIpad }) {
 
             <div className={styles.head}>
                 <span onClick={() => setClose(!close)} className={styles.reset}>Сбросить</span>
-                <span onClick={() => setCatalogButtonIpad(true)}>{svg}</span>
             </div>
 
             <div className={styles.content}>
@@ -40,7 +39,3 @@ export default function CategoryMobile({ setCatalogButtonIpad }) {
         </div>
     )
 }
-
-const svg = <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd" d="M19 6.4L17.6 5L12 10.6L6.4 5L5 6.4L10.6 12L5 17.6L6.4 19L12 13.4L17.6 19L19 17.6L13.4 12L19 6.4Z" fill="#264794" />
-</svg>
