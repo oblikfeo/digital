5141ee7fb25d6c02aa6111c00f9c7718a02e5c0b
Maksim P <email@example.comkfc.kurochka@gmail.com>
2025-02-25 04:37:01 +0600

    full adaptive

--- src/components/nowOrder/nowOrder.tsx
+++ src/components/nowOrder/nowOrder.tsx
@@ -6,6 +6,8 @@ import { useState } from "react"
 export default function NowOrder() {
 
     const [open, setOpen] = useState(false)
+    const [open2, setOpen2] = useState(false)
+    const [open3, setOpen3] = useState(false)
 
     return (
         <>
@@ -22,7 +24,7 @@ export default function NowOrder() {
                     {open && <div className={styles.list}>
                         <div className={styles.item}>
                             <div className={styles.flex}>
-                                <Image src={img} alt="" height={48} width={48} />
+                                <Image src={img} alt="" height={60} width={60} />
                                 <div className={styles.discription}>Суспензия для собак средних и крупных пород Pchelodar Ветспокоин 75 мл</div>
                             </div>
                             <div className={styles.prices}>
@@ -33,7 +35,7 @@ export default function NowOrder() {
                         </div>
                         <div className={styles.item}>
                             <div className={styles.flex}>
-                                <Image src={img} alt="" height={48} width={48} />
+                                <Image src={img} alt="" height={60} width={60} />
                                 <div className={styles.discription}>Суспензия для собак средних и крупных пород Pchelodar Ветспокоин 75 мл</div>
                             </div>
                             <div className={styles.prices}>
@@ -44,7 +46,7 @@ export default function NowOrder() {
                         </div>
                         <div className={styles.item}>
                             <div className={styles.flex}>
-                                <Image src={img} alt="" height={48} width={48} />
+                                <Image src={img} alt="" height={60} width={60} />
                                 <div className={styles.discription}>Суспензия для собак средних и крупных пород Pchelodar Ветспокоин 75 мл</div>
                             </div>
                             <div className={styles.prices}>
@@ -58,14 +60,118 @@ export default function NowOrder() {
                         <div className={styles.text}>Общая сумма заказа:</div>
                         <div className={styles.allPrice}>18 552 ₽</div>
                     </div>}
+                    <button className={styles.repeat2}>Повторить заказ</button>
                 </div>
+
+                <div className={styles.order}>
+                    <div onClick={() => setOpen2(!open2)} className={styles.box}>
+                        <div className={styles.left}>
+                            <h1>Заказ № 001 от 1.01.2001</h1>
+                            <div className={styles.dotes}></div>
+                            {open2 ? up : down}
+                        </div>
+                        <button className={styles.repeat}>Повторить заказ</button>
+                    </div>
+                    {open2 && <div className={styles.list}>
+                        <div className={styles.item}>
+                            <div className={styles.flex}>
+                                <Image src={img} alt="" height={60} width={60} />
+                                <div className={styles.discription}>Суспензия для собак средних и крупных пород Pchelodar Ветспокоин 75 мл</div>
+                            </div>
+                            <div className={styles.prices}>
+                                <h2>6 184 ₽</h2>
+                                <h3>773 ₽ x 8</h3>
+                                <h4>873 ₽</h4>
+                            </div>
+                        </div>
+                        <div className={styles.item}>
+                            <div className={styles.flex}>
+                                <Image src={img} alt="" height={60} width={60} />
+                                <div className={styles.discription}>Суспензия для собак средних и крупных пород Pchelodar Ветспокоин 75 мл</div>
+                            </div>
+                            <div className={styles.prices}>
+                                <h2>6 184 ₽</h2>
+                                <h3>773 ₽ x 8</h3>
+                                <h4>873 ₽</h4>
+                            </div>
+                        </div>
+                        <div className={styles.item}>
+                            <div className={styles.flex}>
+                                <Image src={img} alt="" height={60} width={60} />
+                                <div className={styles.discription}>Суспензия для собак средних и крупных пород Pchelodar Ветспокоин 75 мл</div>
+                            </div>
+                            <div className={styles.prices}>
+                                <h2>6 184 ₽</h2>
+                                <h3>773 ₽ x 8</h3>
+                                <h4>873 ₽</h4>
+                            </div>
+                        </div>
+                    </div>}
+                    {open2 && <div className={styles.summ}>
+                        <div className={styles.text}>Общая сумма заказа:</div>
+                        <div className={styles.allPrice}>18 552 ₽</div>
+                    </div>}
+                    <button className={styles.repeat2}>Повторить заказ</button>
+                </div>
+
+                <div className={styles.order}>
+                    <div onClick={() => setOpen3(!open3)} className={styles.box}>
+                        <div className={styles.left}>
+                            <h1>Заказ № 001 от 1.01.2001</h1>
+                            <div className={styles.dotes}></div>
+                            {open3 ? up : down}
+                        </div>
+                        <button className={styles.repeat}>Повторить заказ</button>
+                    </div>
+                    {open3 && <div className={styles.list}>
+                        <div className={styles.item}>
+                            <div className={styles.flex}>
+                                <Image src={img} alt="" height={60} width={60} />
+                                <div className={styles.discription}>Суспензия для собак средних и крупных пород Pchelodar Ветспокоин 75 мл</div>
+                            </div>
+                            <div className={styles.prices}>
+                                <h2>6 184 ₽</h2>
+                                <h3>773 ₽ x 8</h3>
+                                <h4>873 ₽</h4>
+                            </div>
+                        </div>
+                        <div className={styles.item}>
+                            <div className={styles.flex}>
+                                <Image src={img} alt="" height={60} width={60} />
+                                <div className={styles.discription}>Суспензия для собак средних и крупных пород Pchelodar Ветспокоин 75 мл</div>
+                            </div>
+                            <div className={styles.prices}>
+                                <h2>6 184 ₽</h2>
+                                <h3>773 ₽ x 8</h3>
+                                <h4>873 ₽</h4>
+                            </div>
+                        </div>
+                        <div className={styles.item}>
+                            <div className={styles.flex}>
+                                <Image src={img} alt="" height={60} width={60} />
+                                <div className={styles.discription}>Суспензия для собак средних и крупных пород Pchelodar Ветспокоин 75 мл</div>
+                            </div>
+                            <div className={styles.prices}>
+                                <h2>6 184 ₽</h2>
+                                <h3>773 ₽ x 8</h3>
+                                <h4>873 ₽</h4>
+                            </div>
+                        </div>
+                    </div>}
+                    {open3 && <div className={styles.summ}>
+                        <div className={styles.text}>Общая сумма заказа:</div>
+                        <div className={styles.allPrice}>18 552 ₽</div>
+                    </div>}
+                    <button className={styles.repeat2}>Повторить заказ</button>
+                </div>
+
             </div>
         </>
 
     )
 }
 
-const down = <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+const down = <svg className={styles.svg} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M16.6 8.6001L12 13.2001L7.4 8.6001L6 10.0001L12 16.0001L18 10.0001L16.6 8.6001Z" fill="#C51A1A" />
 </svg>
 
