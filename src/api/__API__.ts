f260caddff5d29e14e492d53077dfc4ab12a4746
Maksim P <email@example.comkfc.kurochka@gmail.com>
2025-02-27 05:01:00 +0600

    try 2

--- src/api/__API__.ts
+++ src/api/__API__.ts
@@ -1,9 +1,17 @@
-'use client'
+'use client'; // Убедитесь, что файл исполняется на клиенте
+
 import axios from 'axios';
+
+let token = '';
+
+if (typeof window !== 'undefined') {
+    // Этот блок будет выполнен только на клиенте
+    token = localStorage.getItem('USER_TOKEN');
+}
 
 export const axiosInstance = axios.create({
     baseURL: 'https://zoo.devsrv.ru',
     headers: {
-        authorization: `Bearer ${localStorage.getItem('USER_TOKEN')}`
+        Authorization: `Bearer ${token}` // Используйте переменную token
     }
 });
