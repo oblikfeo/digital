b245ad426037a0c9856f8795331e6811ac0dd191
Maksim P <email@example.comkfc.kurochka@gmail.com>
2025-04-01 14:55:42 +0600

    out html

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    domains: ["localhost"],
     unoptimized: true,
   },
+  distDir: "out",
 };
 
 export default nextConfig;
